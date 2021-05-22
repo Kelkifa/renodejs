e6ff5c1bf848269533b3c712dc6b4f6ddb74e2d7
Pham Long Huan <huanvip455@gmail.com>
2021-05-22 17:00:06 +0700

    layouting document leftbar

---
+++ client/src/components/DocumentLeftbar.js
@@ -0,0 +1,14 @@
+import '../components/DocumentLeftbar.scss';
+const React = require('react');
+
+class DocumentLeftbar extends React.Component {
+    render() {
+        return (
+            <div id="DocLeftbar" className="DocLeftbar">
+                as
+            </div>
+        )
+    }
+}
+
+export default DocumentLeftbar;
--- client/src/views/Document.js
+++ client/src/views/Document.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import DocumentNavbar from '../components/DocumentNavbar.js';
+import DocumentLeftbar from '../components/DocumentLeftbar.js';
 
 class Document extends React.Component {
     render() {
         return (
-            <div className="mr-t-10">
+            <div id="Document" className="mr-t-10">
                 <DocumentNavbar></DocumentNavbar>
+                <div className="DocCnt">
+                    <DocumentLeftbar></DocumentLeftbar>
+                </div>
             </div>
         )
     }
