eb528685cc353ca5ef077f8c0f4c2175550d1a3f
Pham Long Huan <huanvip455@gmail.com>
2021-05-22 17:42:40 +0700

    building document content

--- client/src/components/DocumentContent.js
+++ client/src/components/DocumentContent.js
@@ -5,15 +5,25 @@ class DocumentContent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            isLoaded: false,
+            data: []
         }
     }
+    componentDidMount() {
+        fetch('/api')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    isLoaded: true,
+                    data: data
+                })
+            })
+    }
+
     render() {
-        var { id } = useParams();
         return (
             <div id="DocCnt" className="DocCnt">
-                <Router>
-                </Router>
+
             </div>
         )
     }
