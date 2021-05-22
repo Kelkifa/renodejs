fe24ba44eac6ce1bc4327507f43676729e3158ad
Pham Long Huan <huanvip455@gmail.com>
2021-05-22 15:58:43 +0700

    fix text size navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,32 +2,31 @@ import React from 'react';
 import './Navbar.scss';
 
 class Navbar extends React.Component {
-    clickHandler() {
-        console.log(1);
-        // document.location = link;
+    clickHandler(link) {
+        document.location = link;
     }
     render() {
         return (
             <div id="Navbar" className="Navbar">
                 <div className="Navbar__list">
-                    <h1 className="Navbar__list__h1">
+                    <h1 className="Navbar__list__h1" onClick={() => { this.clickHandler("/home") }}>
                         Home
                     </h1>
                 </div>
                 <div className="Navbar__list">
-                    <h2 className="Navbar__list__text">
+                    <h2 className="Navbar__list__h2" onClick={() => { this.clickHandler("/test") }}>
                         Test
                     </h2>
-                    <h2 className="Navbar__list__text">
+                    <h2 className="Navbar__list__h2" onClick={() => { this.clickHandler("/anime") }}>
                         Animes
                     </h2>
-                    <h2 className="Navbar__list__text">
+                    <h2 className="Navbar__list__h2" onClick={() => { this.clickHandler("/document") }}>
                         Documents
                     </h2>
-                    <h2 className="Navbar__list__text">
+                    <h2 className="Navbar__list__h2" onClick={() => { this.clickHandler("/word") }}>
                         Words
                     </h2>
-                    <h2 className="Navbar__list__text Navbar__list__text--outline">
+                    <h2 className="Navbar__list__h2 Navbar__list__h2--outline" onClick={() => { this.clickHandler("/login") }}>
                         Sign in
                     </h2>
                 </div>
