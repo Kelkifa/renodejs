4be4e306ea373c2787f9f2c2be35897af95f50ef
Pham Long Huan <huanvip455@gmail.com>
2021-06-12 15:07:15 +0700

    finish

--- client/src/views/Document.js
+++ client/src/views/Document.js
@@ -2,21 +2,26 @@ import '../components/Doc/doc.scss';
 import DocNavbar from '../components/Doc/DocNavbar';
 import Leftbar from '../components/Doc/Leftbar';
 import DocRightContent from '../components/Doc/DocRightContent';
-// import Leftbar from '../components/Doc/Leftbar';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext, createContext } from 'react';
 import { useLocation } from "react-router-dom";
 import queryString from 'query-string';
+
+export const docContext = createContext();
 
 function Document(props) {
     /** Query */
     const getQuery = new URLSearchParams(useLocation().search);
     const query = { type: getQuery.get("type"), id: getQuery.get("id"), update: getQuery.get("update") };
+
     /** State */
     const [success, setSuccess] = useState(false);
     const [message, setMessage] = useState("");
+
     const [types, setTypes] = useState([]);
     const [doc, setDoc] = useState({});
     const [titles, setTitles] = useState([]);
+
+    const [refesh, setRefesh] = useState(true);         // refesh signal, get data form server again
 
     /** Effect Fetch */
     useEffect(() => {
@@ -39,6 +44,11 @@ function Document(props) {
         }
         fetchDocument();
     }, [query.id, query.type, query.update]);
+
+    const docRefeshHandler = () => {
+        setRefesh(!refesh);
+    }
+
     function createRightContentFlag(id, type, update) {     //0: create, 1: update, 2: show, 3: null
         if (type && update) return 1;
         if (type && id) return 2
@@ -52,14 +62,16 @@ function Document(props) {
 
     return (
         <div>
-            <DocNavbar baseLink="/document"
-                data={types} />
-            <div className="doc__body">
-                <Leftbar data={titles}
-                    type={query.type}
-                    baseLink={"/document"} />
-                <DocRightContent doc={doc} type={query.type} flagRightContent={flagRightContent} />
-            </div>
+            <docContext.Provider value={doc, docRefeshHandler}>
+                <DocNavbar baseLink="/document"
+                    data={types} />
+                <div className="doc__body">
+                    <Leftbar data={titles}
+                        type={query.type}
+                        baseLink={"/document"} />
+                    <DocRightContent doc={doc} type={query.type} flagRightContent={flagRightContent} />
+                </div>
+            </docContext.Provider>
         </div>
     );
 }
